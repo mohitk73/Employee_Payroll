44ab6a9046d4efa7d9ed6c8d77b02a5fa5c1a59b
mohitk73 <mohit7740058880@gmail.com>
2025-12-17 16:00:49 +0530

    change funtionality of header active page

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -1,24 +1,24 @@
 <?php
+
 use App\Helpers\Session;
-$activepage = $_GET['route'] ?? "";
+
 Session::start();
+$activepage = basename($_SERVER['REQUEST_URI']);
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>
-        <?php 
-        if (Session::get('role')== 1) {
+        <?php
+        if (Session::get('role') == 1) {
             echo "Admin Dashboard - Mind2Web Payroll";
-        }  elseif(Session::get('role')==0) {
+        } elseif (Session::get('role') == 0) {
             echo "Employee dashboard";
-        }
-        elseif(Session::get('role')==2) {
+        } elseif (Session::get('role') == 2) {
             echo "HR dashboard";
-        }
-        else{
-              echo "Manager Dashboard";
+        } else {
+            echo "Manager Dashboard";
         }
         ?>
     </title>
@@ -28,52 +28,65 @@ Session::start();
 
 <body>
     <div class="sidebar">
-        <h1><i class="fas fa-file-invoice-dollar"></i>Mind2Web Payroll</h1>
+        <div>
+            <h1><i class="fas fa-file-invoice-dollar"></i>Mind2Web Payroll</h1>
+        
         <?php if (Session::get('role') == 1) { ?>
-             <a href="/admin_dashboard" class="<?= $activepage == 'admin_dashboard' ? 'active' : '' ?>">Dashboard</a>
-    <a href="/admin_employees" class="<?= $activepage == 'admin_employees' ? 'active' : '' ?>">Employees</a>
-    <a href="/admin_attendance" class="<?= $activepage == 'admin_attendance' ? 'active' : '' ?>">Attendance</a>
-    <a href="/salarystructure" class="<?= $activepage == 'salarystructure' ? 'active' : '' ?>">Salary Structure</a>
-    <a href="/payroll" class="<?= $activepage == 'payroll' ? 'active' : '' ?>">Payroll</a>
-    <a href="/payslips" class="<?= $activepage == 'payslips' ? 'active' : '' ?>">Payslips</a>
-    <a href="/queries" class="<?= $activepage == 'queries' ? 'active' : '' ?>">Employee Queries</a>
-    <a href="/logout" class="logout">Logout</a>
+            <div>
+                <a href="/admin_dashboard" class="<?= $activepage == 'admin_dashboard' ? 'active' : '' ?>">Dashboard</a>
+                <a href="/admin_employees" class="<?= $activepage == 'admin_employees' ? 'active' : '' ?>">Employees</a>
+                <a href="/admin_attendance" class="<?= $activepage == 'admin_attendance' ? 'active' : '' ?>">Attendance</a>
+                <a href="/salarystructure" class="<?= $activepage == 'salarystructure' ? 'active' : '' ?>">Salary Structure</a>
+                <a href="/payroll" class="<?= $activepage == 'payroll' ? 'active' : '' ?>">Payroll</a>
+                <a href="/payslips" class="<?= $activepage == 'payslips' ? 'active' : '' ?>">Payslips</a>
+                <a href="/queries" class="<?= $activepage == 'queries' ? 'active' : '' ?>">Employee Queries</a>
+            </div>
+            </div>
         <?php } ?>
 
         <?php if (Session::get('role') == 2) { ?>
-            <a href="/hr/dashboard" class="<?= $activepage == '/hr/hrdashboard' ? 'active' : '' ?>">Dashboard</a>
-            <a href="/hr/employees" class="<?= $activepage == '/hr/employees' ? 'active' : '' ?>">Employees</a>
-            <a href="/hr/attendance" class="<?= $activepage == '/hr/attendance' ? 'active' : '' ?>">Attendance</a>
-            <a href="/hr/salarystructure" class="<?= $activepage == '/hr/salarystructure' ? 'active' : '' ?>">Salary Structure</a>
-            <a href="/hr/payroll" class="<?= $activepage == '/hr/payroll' ? 'active' : '' ?>">Payroll</a>
-            <a href="/hr/payslips" class="<?= $activepage == '/hr/payslips' ? 'active' : '' ?>">Payslips</a>
-            <a href="/logout" class="logout">Logout</a>
+        <div>
+            <a href="/hr/dashboard" class="<?= $activepage == 'dashboard' ? 'active' : '' ?>">Dashboard</a>
+            <a href="/hr/employees" class="<?= $activepage == 'employees' ? 'active' : '' ?>">Employees</a>
+            <a href="/hr/attendance" class="<?= $activepage == 'attendance' ? 'active' : '' ?>">Attendance</a>
+            <a href="/hr/salarystructure" class="<?= $activepage == 'salarystructure' ? 'active' : '' ?>">Salary Structure</a>
+            <a href="/hr/payroll" class="<?= $activepage == 'payroll' ? 'active' : '' ?>">Payroll</a>
+            <a href="/hr/payslips" class="<?= $activepage == 'payslips' ? 'active' : '' ?>">Payslips</a>
+        </div>
+        </div>
+       
         <?php } ?>
 
         <?php if (Session::get('role') == 3) { ?>
-            <a href="/manager/dashboard" class="<?= $activepage == 'manager/dashboard' ? 'active' : '' ?>">Dashboard</a>
-            <a href="/manager/employees" class="<?= $activepage == 'manager/employees' ? 'active' : '' ?>">Employees List</a>
-            <a href="/manager/attendance" class="<?= $activepage == 'manager/attendance' ? 'active' : '' ?>">Attendance</a>
-            <a href="/manager/managerpayslip" class="<?= $activepage == 'manager/payslip' ? 'active' : '' ?>"> Your Payslip</a>
-            <a href="/manager/contactsupport" class="<?= $activepage == 'manager/contactsupport' ? 'active' : '' ?>">Contact Support</a>
-            <a href="/logout" class="logout" class="<?= $activepage == '/logout' ? 'active' : '' ?>">Logout</a>
+        <div>
+            <a href="/manager/dashboard" class="<?= $activepage == 'dashboard' ? 'active' : '' ?>">Dashboard</a>
+            <a href="/manager/employees" class="<?= $activepage == 'employees' ? 'active' : '' ?>">Employees List</a>
+            <a href="/manager/attendance" class="<?= $activepage == 'attendance' ? 'active' : '' ?>">Attendance</a>
+            <a href="/manager/managerpayslip" class="<?= $activepage == 'payslip' ? 'active' : '' ?>"> Your Payslip</a>
+            <a href="/manager/contactsupport" class="<?= $activepage == 'contactsupport' ? 'active' : '' ?>">Contact Support</a>
+        </div>
+        </div>
+       
         <?php } ?>
 
         <?php if (Session::get('role') == 0) { ?>
-            <a href="/employee/dashboard" class="<?= $activepage == 'employee/dashboard' ? 'active' : '' ?>">Dashboard</a>
-            <a href="/employee/attendance" class="<?= $activepage == 'employee/attendance' ? 'active' : '' ?>">Attendance</a>
-            <a href="/employee/emppayslip" class="<?= $activepage == 'employee/emppayslip' ? 'active' : '' ?>">Payslips</a>
-            <a href="/employee/contactsupport" class="<?= $activepage == 'employee/contactsupport' ? 'active' : '' ?>">Contact Support</a>
             <div>
-                <a href="/logout" class="logout">Logout</a>
+                <a href="/employee/dashboard" class="<?= $activepage == 'dashboard' ? 'active' : '' ?>">Dashboard</a>
+                <a href="/employee/attendance" class="<?= $activepage == 'attendance' ? 'active' : '' ?>">Attendance</a>
+                <a href="/employee/emppayslip" class="<?= $activepage == 'emppayslip' ? 'active' : '' ?>">Payslips</a>
+                <a href="/employee/contactsupport" class="<?= $activepage == 'contactsupport' ? 'active' : '' ?>">Contact Support</a>
+            </div>
             </div>
         <?php } ?>
+         <div>
+                <a href="/logout" class="logout">Logout</a>
+            </div>
     </div>
     <main>
         <header>
 
             <div>
-               
+
             </div>
             <div class="nav">
                 <h2>Welcome <?= Session::get('name') ?>!</h2>
@@ -83,8 +96,8 @@ Session::start();
                     <nav>
                         <ul>
                             <li><a href="/profile">View Profile</a></li>
-                            <?php if($_SESSION['role']==1){ ?><li><a href="/queries">View Queries</a></li><?php } else{?>
-                                <li><a href="/employee/contactsupport">Get Help</a></li><?php }?>
+                            <?php if ($_SESSION['role'] == 1) { ?><li><a href="/queries">View Queries</a></li><?php } else { ?>
+                                <li><a href="/employee/contactsupport">Get Help</a></li><?php } ?>
                             <li><a href="/logout">Logout</a></li>
                         </ul>
                     </nav>
